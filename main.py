a86b85c49f5c6ccff21cc381abba00613b9c1904
Jonathan Remmers <jona.remm@gmail.com>
2018-11-20 17:55:17 +0100

    changing input method to reading from a file

--- main.py
+++ main.py
@@ -1,37 +1,25 @@
-# mtg breaker v0.0.1
-
 def main():
     #
     print("MTG Breaker for Rough Drafts")
-    players = get_players()
-    print(players)
+    print("Enter file name (without file extension):")
+    file_name = input("--> ")
+    try:
+        with open(file_name + ".txt") as f:
+            for line in f:
+                process(line)
+    except FileNotFoundError:
+        print("File not found.")
+    except:
+        print("Something went wrong.")
 
-def get_players():
-    # get a list of all players
-    print("Please enter the names of all players attending as a comma separated list.")
-    players = input("--> ")
-    return players.split(", ")
+def process(line):
+    # name: 2, name: 1
+    x = line.split(", ")
 
-def tiebreakers(player_list):
-    # calculate tiebreakers for all players
-    pass
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.opponents = []
 
-def pmw(match_points, rounds_played):
-    # calculate player match win percentage
-    return match_points / (rounds_played * 3)
-
-def omw(arg):
-    # calculate opponent match win percentage
-    # omw% is the average of the pmw% of each of their opponents
-    pass
-
-def pgw(games_won, games_played):
-    # calculate player game win percentage
-    return games_won / games_played
-
-def ogw(arg):
-    # calculate opponent game win percentage
-    # ogw% is the average of the pgw% of each of their opponents
-    pass
 
 main()
