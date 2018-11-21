55e903be72955ecddd90020ba60e8ab6e392f16f
Jonathan Remmers <jona.remm@gmail.com>
2018-11-21 12:38:07 +0100

    get a list of all players' names

--- main.py
+++ main.py
@@ -1,22 +1,31 @@
+all_players = []
+
 def main():
     #
+
+
     print("MTG Breaker for Rough Drafts")
     print("Enter file name:")
     file_name = input("--> ")
-    try:
-        with open(file_name) as f:
-            for line in f:
-                process(line)
-    except FileNotFoundError:
-        print("File not found.")
-    except:
-        print("Something went wrong.")
+    with open(file_name) as f:
+        for line in f:
+            process(line.strip())
+
+    print(all_players)
 
 def process(line):
-    # name: 2, name: 1
-    x = line.split(", ")
+    # line -> "name: 2, name: 1"
+    get_players(line)
 
-class Player:
+def get_players(line):
+    player_and_score = line.split(", ")
+    for i in player_and_score:
+        player_name = i[:-3]
+        if player_name not in all_players:
+            all_players.append(player_name)
+
+
+class Player(object):
     def __init__(self, name):
         self.name = name
         self.opponents = []
